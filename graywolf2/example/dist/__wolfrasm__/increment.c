b405798f81c5e2a3ea10c69170364417d1a777af
Muttaqi <39924187+muttaqi@users.noreply.github.com>
2021-06-07 23:46:50 -0700

    Ran GW once

---
+++ graywolf2/example/dist/__wolfrasm__/increment.c
@@ -0,0 +1,40 @@
+#include "math.h"
+
+#include "WolframRTL.h"
+
+static WolframCompileLibrary_Functions funStructCompile;
+
+static void * E0 = 0;
+
+
+static mbool initialize = 1;
+
+
+DLLEXPORT int Initialize_increment(WolframLibraryData libData)
+{
+if( initialize)
+{
+funStructCompile = libData->compileLibraryFunctions;
+{
+E0 = funStructCompile->getExpressionFunctionPointer(libData, "Hold[Function[List[args$], increment[ReplaceAll[args$, Rule[List, Squence]]]]]");
+}
+if( E0 == 0)
+{
+return LIBRARY_FUNCTION_ERROR;
+}
+initialize = 0;
+}
+return 0;
+}
+
+DLLEXPORT void Uninitialize_increment(WolframLibraryData libData)
+{
+if( !initialize)
+{
+initialize = 1;
+}
+}
+
+DLLEXPORT mreal increment( mreal A1) {
+return 1 + A1;
+}
